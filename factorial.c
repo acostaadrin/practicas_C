8ca44cbe1b41a1077413e62e804b37cf27a23de2
Adrian Acosta <acostaadrin199@gmail.com>
2023-03-01 13:36:25 -0300

    Actualización 01-03-2023

--- factorial.c
+++ factorial.c
@@ -5,17 +5,16 @@
 int factorial(int num);
 
 void main(){
-	int num1, num2;
+	int num1;
 	printf("Ingrese un numero: ");
 	scanf("%d", &num1);
 	printf("El factorial del numero ingresado es: %d\n", factorial(num1));	
 }
 
-int factorial(num){
+int factorial(int num){
 	int i, factorial = 1;
 	for (i = 0; i < num; i++){
 		factorial = factorial * (num - i);
 	}
 	return factorial;
 }
-
